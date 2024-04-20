b14e5bcfdc3a28c6f2faff30377fb451fac5452d
Ivan Vuong <vuongi2@uci.edu>
2024-04-20 12:19:12 -0700

    small fixes

--- app/survey/page.js
+++ app/survey/page.js
@@ -4,8 +4,6 @@ import React, { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { Button } from "@mui/material";
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
 export default function Home() {
     const [_type, setType] = useState("");
     const [_time_diagnosis, setTimeDiagnosis] = useState("");
