937ab620095ac1501131bb7ed217e3c850081afe
Efe <suleymanefeclk2@gmail.com>
2024-04-20 14:39:11 -0700

    🎨 UI: Egg animation

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,8 +1,89 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import useUser from "../../hooks/useUser";
+import Image from "next/image";
 
-export default function Home() {
-  return <div className="w-full">hey</div>;
+export default function Dashboard() {
+  const [isShaking, setIsShaking] = useState(false);
+  const [currentEggStage, setCurrentEggStage] = useState(1);
+
+  //   Egg shaking animation
+  useEffect(() => {
+    const shake = () => {
+      setIsShaking(true);
+      setTimeout(() => {
+        setIsShaking(false);
+      }, 820);
+    };
+
+    const startRandomShake = () => {
+      const randomInterval =
+        Math.random() * 4000 + 1000 - currentEggStage * 100;
+
+      setTimeout(() => {
+        shake();
+        startRandomShake();
+      }, randomInterval);
+    };
+
+    const updateStageBasedOnTime = () => {
+      const currentTime = new Date();
+      const currentHours = currentTime.getHours();
+
+      if (currentHours < 10) {
+        setCurrentEggStage(1);
+      } else if (currentHours < 12) {
+        setCurrentEggStage(2);
+      } else if (currentHours < 14) {
+        setCurrentEggStage(3);
+      } else if (currentHours < 16) {
+        setCurrentEggStage(4);
+      } else if (currentHours < 18) {
+        setCurrentEggStage(6);
+      } else if (currentHours < 20) {
+        setCurrentEggStage(7);
+      } else {
+        setCurrentEggStage(8);
+      }
+    };
+
+    updateStageBasedOnTime();
+    startRandomShake();
+
+    return () => clearTimeout(startRandomShake);
+  }, []);
+
+  return (
+    <div className="w-full h-screen">
+      <div className="container mx-auto">
+        {/* Top NavBar */}
+        <div className="w-full">
+          <p>.</p>
+        </div>
+
+        {/* Egg Visualization */}
+        <div className="flex justify-center bg-card shadow-xl rounded-md items-center object-fill mt-32">
+          <div className="p-8">
+            <div
+              className={isShaking ? "shake-animation" : ""}
+              style={{
+                "--shake-x-negative": `-${currentEggStage * 0.6}px`,
+                "--shake-x-positive": `${currentEggStage * 0.6}px`,
+              }}
+            >
+              <Image
+                src={`/eggs2/egg_${currentEggStage}.png`}
+                alt="Egg"
+                width={142}
+                height={168}
+                className="object-fill"
+                layout="fixed"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
